ace1e8d75afd71da71da40f436e39c5d763e8b1d
Gendou_OMG <studfrom2005@gmail.com>
2020-04-15 18:01:53 +0300

    Обновление

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+var myMap;
+ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
+function init () {
+	myMap = new ymaps.Map("map_canvas", {
+		center: [55.76, 37.64], // Координаты центра карты
+		zoom: 10 // Zoom
+    });
+}
+
